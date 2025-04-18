afdd90935bdcc81ca174c0f18791d161288ffdf1
Stepan Agafonov <agafonov.stepan@gmail.com>
2025-04-18 20:04:53 +0300

    test

--- app/agents/evaluation_agent.py
+++ app/agents/evaluation_agent.py
@@ -1,6 +1,6 @@
 from agents import Agent
 from app.agents.tools.extract_situation import extract_situation
-from app.agents.tools.extract_task import extract_task
+from app.agents.tools.extract_task import extract_taskgit 
 from app.agents.tools.extract_action import extract_action
 from app.agents.tools.extract_result import extract_result
 from pydantic import BaseModel
@@ -15,7 +15,7 @@ class STAROutput(BaseModel):
 def create_evaluation_agent(system_prompt: str) -> Agent:
     return Agent(
         name="Evaluation Agent",
-        handoff_description="Оценка сообщений с использованием метода STAR.",
+        handoff_description="Оценка сообщений с использованием мето STAR.",
         instructions=system_prompt,
         tools=[extract_situation, extract_action, extract_task, extract_result],
         output_type=STAROutput
